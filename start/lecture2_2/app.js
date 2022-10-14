dfb35c5d8a3773c739d34d1a37f4f710fffa0c52
Marcus Tyler-Moore <marcus.a.tyler-moore@bofa.com>
2022-10-14 22:56:31 +0100

    Lecture2_2_1

---
+++ start/lecture2_2/app.js
@@ -0,0 +1,30 @@
+import * as THREE from '../../libs/three/three.module.js';
+import { OrbitControls } from '../../libs/three/jsm/OrbitControls.js';
+
+class App{
+	constructor(){
+		const container = document.createElement( 'div' );
+		document.body.appendChild( container );
+    
+        this.camera = new THREE.PerspectiveCamera(60, window.innerWidth/window.innerHeight,0.1,100);
+		this.camera.position.set(0,0,4);
+		this.scene = new THREE.Scene();
+		this.scene.background = new THREE.Color(0xaaaaaa);
+		this.renderer = new THREE.WebGLRenderer({antialias: true});
+		this.renderer.setPixelRatio(window.devicePixelRatio);
+		this.renderer.setSize(this.renderer.domElement);
+		container.appendChild(this.render.bind(this));
+		this.renderer.setAnimationLoop(this.render.bind(this));
+        window.addEventListener('resize', this.resize.bind(this) );
+	}	
+    
+    resize(){
+        
+    }
+    
+	render( ) {  
+        this.renderer.render(this.scene, this.camera);
+    }
+}
+
+export { App };

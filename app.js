373a2a36bf01de38f11b722cf7966636370a0c9a
Marcus Tyler-Moore <marcus.a.tyler-moore@bofa.com>
2022-10-14 23:04:58 +0100

    corrected app.js

--- app.js
+++ app.js
@@ -8,12 +8,14 @@ class App{
     
         this.camera = new THREE.PerspectiveCamera(60, window.innerWidth/window.innerHeight,0.1,100);
 		this.camera.position.set(0,0,4);
+
 		this.scene = new THREE.Scene();
 		this.scene.background = new THREE.Color(0xaaaaaa);
 		this.renderer = new THREE.WebGLRenderer({antialias: true});
+
 		this.renderer.setPixelRatio(window.devicePixelRatio);
-		this.renderer.setSize(this.renderer.domElement);
-		container.appendChild(this.render.bind(this));
+		this.renderer.setSize(window.innerWidth, window.innerHeight);
+		container.appendChild(this.renderer.domElement);
 		this.renderer.setAnimationLoop(this.render.bind(this));
         window.addEventListener('resize', this.resize.bind(this) );
 	}	
